c367f411a69bff806195bbfa22fc78edf31d6fd1
jarrodwatts <jarrod@thirdweb.com>
2024-08-28 18:16:28 +1000

    Working mint from frontend

--- smart-contract-account-factory/frontend/components/DeploySmartWalletSection.tsx
+++ smart-contract-account-factory/frontend/components/DeploySmartWalletSection.tsx
@@ -5,12 +5,36 @@ import { Button } from "./ui/button";
 import { useAddress } from "@/lib/useAddress";
 import {
   FACTORY_CONTRACT_ADDRESS,
-  FACTORY_CONTRACT_ADDRESS_ABI,
+  FACTORY_ABI,
+  NFT_ABI,
+  NFT_ADDRESS,
+  NFT_PAYMASTER_ADDRESS,
+  VALIDATOR_ADDRESS,
 } from "@/const/contracts";
 import useWalletClient from "@/lib/useWalletClient";
 import { abstractTestnet } from "viem/chains";
-import { createPublicClient, Hex, http } from "viem";
+import {
+  createPublicClient,
+  createWalletClient,
+  encodeAbiParameters,
+  encodeFunctionData,
+  Hex,
+  http,
+  parseAbiParameters,
+} from "viem";
 import Link from "next/link";
+import {
+  toSmartAccount,
+  eip712WalletActions,
+  getGeneralPaymasterInput,
+  serializeTransaction,
+} from "viem/zksync";
+import {
+  generateRandom32ByteHex,
+  getSignInput,
+  getTypedDataDomain,
+  typedDataTypes,
+} from "@/lib/utils";
 
 export default function DeploySmartWalletSection() {
   const walletClient = useWalletClient();
@@ -39,11 +63,14 @@ export default function DeploySmartWalletSection() {
                 chain: abstractTestnet,
               });
 
+              // Generate a random salt
+              const salt = generateRandom32ByteHex();
+
               const transactionHash = await walletClient?.writeContract({
-                abi: FACTORY_CONTRACT_ADDRESS_ABI,
+                abi: FACTORY_ABI,
                 address: FACTORY_CONTRACT_ADDRESS,
                 functionName: "deployAccount",
-                args: [address],
+                args: [address, salt.toString()],
                 account: address,
                 chain: abstractTestnet,
               });
@@ -63,6 +90,8 @@ export default function DeploySmartWalletSection() {
                 await publicClient.waitForTransactionReceipt({
                   hash: transactionHash,
                 });
+
+              console.log("Transaction details: ", transactionDetails);
 
               setSmartContractAccountAddress(
                 transactionDetails.contractAddress as Hex
@@ -86,6 +115,93 @@ export default function DeploySmartWalletSection() {
               </Link>
             </p>
           )}
+        </>
+      )}
+
+      {smartContractAccountAddress && (
+        <>
+          <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-center mt-12">
+            3. Submit Transaction from Smart Contract Account
+          </h2>
+
+          <p className="leading-7 mt-2 text-center">
+            Mint an NFT from the smart contract account.
+          </p>
+
+          <Button
+            className="w-full max-w-[256px] mt-2"
+            onClick={async () => {
+              const smartAccount = toSmartAccount({
+                address: smartContractAccountAddress, // The address of the deployed smart contract account
+                async sign({ hash }) {
+                  const result = await walletClient?.signMessage({
+                    message: hash,
+                    account: address as Hex,
+                  });
+                  return result as Hex;
+                },
+              });
+              const smartAccountClient = createWalletClient({
+                account: smartAccount, // Use the smart contract account
+                chain: abstractTestnet,
+                transport: http(`https://api.testnet.abs.xyz`),
+              }).extend(eip712WalletActions());
+
+              const mintData = encodeFunctionData({
+                abi: NFT_ABI,
+                functionName: "mint",
+                args: [smartContractAccountAddress, 1],
+              });
+
+              // Prepare a transaction to mint an NFT using the smart account
+              const preppedTx =
+                await smartAccountClient.prepareTransactionRequest({
+                  account: smartContractAccountAddress as Hex,
+                  chain: abstractTestnet,
+                  data: mintData,
+                  paymaster: NFT_PAYMASTER_ADDRESS,
+                  paymasterInput: getGeneralPaymasterInput({
+                    innerInput: "0x",
+                  }),
+                  to: NFT_ADDRESS,
+                  type: "eip712",
+                });
+
+              // Convert the prepped transaction into a typed data object
+              const typedData = {
+                types: typedDataTypes(),
+                primaryType: "Transaction",
+                message: getSignInput(preppedTx as any), // Reshape the object into Eip-712 format
+                domain: getTypedDataDomain(),
+              };
+
+              // The EOA signs the typed data under the hood
+              const rawSignature = await smartAccountClient.signTypedData(
+                typedData as any
+              );
+
+              const signature = encodeAbiParameters(
+                parseAbiParameters(["bytes", "address", "bytes[]"]),
+                [rawSignature as `0x${string}`, VALIDATOR_ADDRESS, []]
+              );
+
+              // Finally, format a transaction where it's sent from the smart contract
+              // But the signature is from the EOA that is the signer of the account
+              const serializedTx = serializeTransaction({
+                ...preppedTx,
+                customSignature: signature,
+              } as any);
+
+              const transactionHash =
+                await smartAccountClient.sendRawTransaction({
+                  serializedTransaction: serializedTx,
+                });
+
+              console.log("Transaction hash: ", transactionHash);
+            }}
+          >
+            Mint NFT
+          </Button>
         </>
       )}
     </div>
--- smart-contract-account-factory/frontend/lib/utils.ts
+++ smart-contract-account-factory/frontend/lib/utils.ts
@@ -1,6 +1,72 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { TransactionRequest } from "viem";
+import { abstractTestnet } from "viem/chains";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateRandom32ByteHex() {
+  let result = "0x";
+  const hexChars = "0123456789abcdef";
+  for (let i = 0; i < 64; i++) {
+    result += hexChars[Math.floor(Math.random() * 16)];
+  }
+  return result;
+}
+
+export type AbstractTransactionRequest = TransactionRequest & {
+  paymasterInput: string;
+  paymaster: string;
+};
+
+export function getSignInput(transaction: AbstractTransactionRequest) {
+  const tx = {
+    txType: 113n,
+    from: transaction.from!,
+    to: transaction.to,
+    gasLimit: transaction.gas,
+    gasPerPubdataByteLimit: 50_000n,
+    maxFeePerGas: transaction.maxFeePerGas,
+    maxPriorityFeePerGas: transaction.maxPriorityFeePerGas,
+    paymaster: transaction.paymaster,
+    nonce: transaction.nonce,
+    value: 0,
+    data: transaction.data,
+    factoryDeps: [],
+    paymasterInput: transaction.paymasterInput,
+  };
+
+  console.log(tx);
+
+  return tx;
+}
+
+export function typedDataTypes() {
+  return {
+    Transaction: [
+      { name: "txType", type: "uint256" },
+      { name: "from", type: "uint256" },
+      { name: "to", type: "uint256" },
+      { name: "gasLimit", type: "uint256" },
+      { name: "gasPerPubdataByteLimit", type: "uint256" },
+      { name: "maxFeePerGas", type: "uint256" },
+      { name: "maxPriorityFeePerGas", type: "uint256" },
+      { name: "paymaster", type: "uint256" },
+      { name: "nonce", type: "uint256" },
+      { name: "value", type: "uint256" },
+      { name: "data", type: "bytes" },
+      { name: "factoryDeps", type: "bytes32[]" },
+      { name: "paymasterInput", type: "bytes" },
+    ],
+  };
+}
+
+export function getTypedDataDomain() {
+  return {
+    name: "zkSync",
+    version: "2",
+    chainId: abstractTestnet.id,
+  };
 }
