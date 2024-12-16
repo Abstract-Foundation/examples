5cf31c6bb7904a12dd4d5a13725df0accf84efd4
Anton Kenikh <kenikh@me.com>
2024-12-16 18:38:30 +0400

    fix(layerzero-onft): config import causes running connect-contracts script

---
+++ layerzero-onft/deploy/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  baseSepolia: {
+    endpointId: 40245,
+    // Replace with your own NFT contract address
+    nftContractAddress: "0x64D71429d50172CD3B759126C4C43a3dA8B0a038",
+  },
+  abstractTestnet: {
+    endpointId: 40313,
+    // Replace with your own NFT contract address
+    nftContractAddress: "0x8442F3845C460AA33eDB2837A9daf627C4D0EfF2",
+  },
+};
