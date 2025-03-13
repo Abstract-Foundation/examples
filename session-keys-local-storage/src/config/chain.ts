fdcc3fefceee6994dd6389cfe2f2fe9484819bf2
Jarrod Watts <jarrod@cubelabs.xyz>
2025-03-13 21:17:55 +1100

    update chains

--- session-keys-local-storage/src/config/chain.ts
+++ session-keys-local-storage/src/config/chain.ts
@@ -3,9 +3,10 @@ import { abstractTestnet, abstract } from "viem/chains";
 /**
  * This exports the chain configuration to be used in the application.
  * Uses Abstract testnet in development and the mainnet in production.
+ * In this simple example, we use the testnet in both environments.
  */
 const chain =
-  process.env.NODE_ENV === "development" ? abstractTestnet : abstract;
+  process.env.NODE_ENV === "development" ? abstractTestnet : abstractTestnet;
 
 export type SupportedChain = typeof chain;
 
